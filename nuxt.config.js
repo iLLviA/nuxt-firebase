2cd1828e5162cc8129caa7a5606b53298f58a8b5
iLLviA <illvia.3594@gmail.com>
2019-05-26 04:10:50 +0900

    wip

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+  // srcDir: 'app',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+
+    { src: '~plugins/element-ui', ssr: false }
+
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    baseURL: 'https://nuxt-firebase-5b1c9.firebaseio.com'
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+   vendor: ['element-ui'],
+
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
